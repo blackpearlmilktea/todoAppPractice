c8dfa24e3e261f9f3efbd51e7929d3009bc683ab
BlackPearlMilkTea <blackpearlmilktea@gmail.com>
2015-09-05 12:54:43 -0700

    Merge branch 'jquery-mustache'
    Conflicts:
    	app_view.js
    	todo_item_view.js

--- app_view.js
+++ app_view.js
@@ -11,20 +11,20 @@ AppView.prototype.load = function(){
 
 //can remove unneeded comments when code explains clearly
 // Events for Textfield and Remove Completed button
-AppView.prototype.addEvents = function() {
-  // be consistent about dash and underscore naming
-  document.getElementById("todo-entry").addEventListener("keypress", this.enterToDo.bind(this));
-  document.getElementById("remove_button").addEventListener("click", this.removeCompleted.bind(this));
+// be consistent about dash and underscore naming
+AppView.prototype.addEvents = function(){
+  $("#todo-entry").on("keypress", this.enterToDo.bind(this));
+  $("#remove_completed").on("click", this.removeCompleted.bind(this));
 };
 
 // function for adding new items when they are entered in textfield
 AppView.prototype.enterToDo = function(e) {
-  var todoText = document.getElementById("todo-entry").value.trim();
+  var todoText = $("#todo-entry").val().trim();
   var ENTER_KEY = 13;
   if (e.keyCode === ENTER_KEY && todoText !== ""){
     // AppView tells todoListView to add an item
     this.todoListView.addItem(todoText);
-    document.getElementById("todo-entry").value = "";
+    $("#todo-entry").val("");
   }
 };
 
--- todo_item_view.js
+++ todo_item_view.js
@@ -1,60 +1,61 @@
+// TODO:
+// Merge the jquery branch to master
+// Then try merging the comment branch to master
+// Use underscore to replace your search and replace code.
+// Then use mustache templates to replace HTML creating code.
+// Read about CSS selector speed.
+// SUPER MEGA EXTRA CREDIT make a branch that converts to Backbone!!!!
+
+
 // View of an Individual Todo Item
 // TODO standardize on the name "todo" or "todoItem"
 var TodoItemView = function(todoItem) {
   this.todo = todoItem;
-  this.template = this.getTemplate();
+  this.$element = this.$buildElement();
   this.addEvents();
   return this;
 };
 
-//TODO This is a place where Mustache templates are useful.
-TodoItemView.prototype.getTemplate = function() {
-  var todoView = document.createElement("li");
-  todoView.setAttribute("data-id", this.todo.id);
-  todoView.innerHTML = '<input type="checkbox"/><label></label>';
-  var checkbox = todoView.getElementsByTagName("input")[0];
-  var label = todoView.getElementsByTagName("label")[0];
+// TODO This is a place where Mustache templates are useful.
+TodoItemView.prototype.$buildElement = function() {
+  var $todoView = $('<li><input type="checkbox"/><label></label></li>')
+    .attr("data-id", this.todo.id);
+
   if(this.todo.completed) {
-    checkbox.setAttribute("checked", true);
-    todoView.setAttribute("class", "complete");
+    $todoView.find("input").attr("checked", true);
+    $todoView.attr("class", "complete");
   };
-  label.appendChild(document.createTextNode(this.todo.title));
-  return todoView;
+
+  $todoView.find("label").text(this.todo.title);
+  return $todoView;
 };
 
 TodoItemView.prototype.addEvents = function() {
-  var checkbox = this.template.getElementsByTagName("input")[0];
-  var label = this.template.getElementsByTagName("label")[0];
-  checkbox.addEventListener("click", this.toggleComplete.bind(this));
-  label.addEventListener("dblclick", this.editText.bind(this));
+  this.$element.find("input").on("click", this.toggleComplete.bind(this));
+  this.$element.find("label").on("dblclick", this.editText.bind(this));
 };
 
 //function for listening for checkbox event to change look of item
 TodoItemView.prototype.toggleComplete = function() {
-  var checkbox = this.template.getElementsByTagName("input")[0];
-  if (checkbox.checked) {
-    this.template.setAttribute("class", "complete");
-    this.todo.toggleCompleted();
-  }
-  else {
-    this.template.removeAttribute("class", "complete");
-    this.todo.toggleCompleted();
-  }
+  this.$element.toggleClass("complete");
+  this.todo.toggleCompleted();
 };
 
 // function for changing todo text
 TodoItemView.prototype.editText = function() {
-  this.template.innerHTML = '<input type="text" value="' + this.todo.title + '"></input>';
-  var editField = this.template.getElementsByTagName("input")[0];
+  // TODO This is an injection attack
+  // This is where to use mustache
+  this.$element.html('<input type="text" value="' + this.todo.title + '"></input>');
+  var editField = this.$element.find("input");
   editField.focus();
-  editField.addEventListener("keypress", this.enterNewTodo.bind(this));
-  editField.addEventListener("blur", this.removeEditing.bind(this));
+  editField.on("keypress", this.enterNewTodo.bind(this));
+  editField.on("blur", this.removeEditing.bind(this));
 };
 
 // function for entering in the new todo text upon Enter key press
 TodoItemView.prototype.enterNewTodo = function(e) {
-  var editField = this.template.getElementsByTagName("input")[0];
-  var newTodoText = editField.value.trim();
+  var editField = this.$element.find("input");
+  var newTodoText = editField.val().trim();
   var ENTER_KEY = 13;
   if (e.keyCode === ENTER_KEY && newTodoText !== "") {
     this.todo.editItemText(newTodoText);
@@ -63,6 +64,6 @@ TodoItemView.prototype.enterNewTodo = function(e) {
 };
 
 TodoItemView.prototype.removeEditing = function() {
-  this.template.innerHTML = this.getTemplate().innerHTML;
+  this.$element.replaceWith(this.$buildElement());
   this.addEvents();
 };
