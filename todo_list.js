b5c4325d15058f876657d617b3abed65596fcb2a
BlackPearlMilkTea <blackpearlmilktea@gmail.com>
2015-09-10 11:16:51 -0700

    Begin converting todo_list into a backbone collection

--- todo_list.js
+++ todo_list.js
@@ -1,26 +1,30 @@
 // Model for a group of Todo items
-var TodoList = function() {
-  this.todos = [];
-  return this;
-};
+// var TodoList = function() {
+//   this.todos = [];
+//   return this;
+// };
 
-TodoList.prototype.addTodo = function(title) {
-  var newTodo = new Todo(title);
-  this.todos.push(newTodo);
-};
+// TodoList.prototype.addTodo = function(title) {
+//   var newTodo = new Todo(title);
+//   this.todos.push(newTodo);
+// };
 
-TodoList.prototype.removeTodo = function(todo) {
-  this.todos = _.without(this.todos, todo);
-};
+// TodoList.prototype.removeTodo = function(todo) {
+//   this.todos = _.without(this.todos, todo);
+// };
 
-TodoList.prototype.getListLength = function() {
-  return this.todos.length;
-};
+// TodoList.prototype.getListLength = function() {
+//   return this.todos.length;
+// };
 
-TodoList.prototype.getNthTodo = function(n) {
-  return this.todos[n];
-};
+// TodoList.prototype.getNthTodo = function(n) {
+//   return this.todos[n];
+// };
 
-TodoList.prototype.removeCompletedTodos = function() {
-  this.todos = _.filter(this.todos, function(todo){ return !todo.completed });
-};
+// TodoList.prototype.removeCompletedTodos = function() {
+//   this.todos = _.filter(this.todos, function(todo){ return !todo.completed });
+// };
+
+var TodoList = Backbone.Collection.extend({
+  model: Todo
+});
