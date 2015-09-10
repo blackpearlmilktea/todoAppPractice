692325d811f6bc1bb8cf6e156d00bcd7334ce228
BlackPearlMilkTea <blackpearlmilktea@gmail.com>
2015-09-10 11:16:30 -0700

    Convert Main App view into Backbone View and have it load the input field and remove button

--- app_view.js
+++ app_view.js
@@ -1,31 +1,39 @@
 // Main App View
-var AppView = function() {
-  this.todoListView = new TodoListView();
-  return this;
-};
+var AppView = Backbone.View.extend({
+  el: '#todo-list-app',
 
-AppView.prototype.load = function() {
-  this.todoListView.render();
-  this.addEvents();
-};
+  template: '<h1>Todo List</h1>' +
+            '<input type="text" id="todo-entry" placeholder="Enter a todo item"/>' +
+            '<div id="todo-list-container"></div>' +
+            '<button id="remove-button">Remove completed items</button>',
 
-AppView.prototype.addEvents = function() {
-  $("#todo-entry").on("keypress", this.enterTodo.bind(this));
-  $("#remove-button").on("click", this.removeCompleted.bind(this));
-};
+  initialize: function(){
+    this.todoListView = new TodoListView();
+    this.render();
+  },
 
-AppView.prototype.enterTodo = function(e) {
-  var todoTitle = $("#todo-entry").val().trim();
-  var ENTER_KEY = 13;
-  if (e.keyCode === ENTER_KEY && todoTitle !== "") {
-    this.todoListView.addTodo(todoTitle);
-    $("#todo-entry").val("");
+  render: function(){
+    this.$el.html(this.template);
+    this.todoListView.render();
+  },
+
+  events: {
+    'keypress #todo-entry': 'enterTodo',
+    'click #remove-button': 'removeCompleted'
+  },
+
+  enterTodo: function(e){
+    var todoTitle = $("#todo-entry").val().trim();
+    var ENTER_KEY = 13;
+    if (e.keyCode === ENTER_KEY && todoTitle !== "") {
+      this.todoListView.addTodo(todoTitle);
+      $("#todo-entry").val("");
+    }
+  },
+
+  removeCompleted: function(){
+    this.todoListView.removeCompletedTodos();
   }
-};
-
-AppView.prototype.removeCompleted = function() {
-  this.todoListView.removeCompletedTodos();
-};
+});
 
 var mainView = new AppView();
-mainView.load();
