619f4cde5f904599f16d64519aded7c8f7ecb7a4
BlackPearlMilkTea <blackpearlmilktea@gmail.com>
2015-09-10 11:18:01 -0700

    Start converting the views into backbone views.

--- todo_list_view.js
+++ todo_list_view.js
@@ -1,28 +1,44 @@
 // View for the Todo List
-var TodoListView = function() {
-  this.todoList = new TodoList();
-  return this;
-};
 
-TodoListView.prototype.render = function() {
-  $("#todo-list").html("");
-  for(var i = 0; i < this.todoList.getListLength(); i++) {
-    var todoView = new TodoView(this.todoList.getNthTodo(i));
-    $("#todo-list").append(todoView.$element);
+var TodoListView = Backbone.View.extend({
+  el: '#todo-list-container',
+
+  render: function(){
+
+  },
+  addTodo: function(todoTitle){
+
+  },
+  removeCompletedTodos: function(){
+
   }
-};
+});
 
-TodoListView.prototype.addTodo = function(title) {
-  this.todoList.addTodo(title);
-  this.render();
-};
 
-TodoListView.prototype.removeTodo = function(todo) {
-  this.todoList.removeTodo(todo);
-  this.render();
-};
+// var TodoListView = function() {
+//   this.todoList = new TodoList();
+//   return this;
+// };
 
-TodoListView.prototype.removeCompletedTodos = function() {
-  this.todoList.removeCompletedTodos();
-  this.render();
-};
+// TodoListView.prototype.render = function() {
+//   $("#todo-list").html("");
+//   for(var i = 0; i < this.todoList.getListLength(); i++) {
+//     var todoView = new TodoView(this.todoList.getNthTodo(i));
+//     $("#todo-list").append(todoView.$element);
+//   }
+// };
+
+// TodoListView.prototype.addTodo = function(title) {
+//   this.todoList.addTodo(title);
+//   this.render();
+// };
+
+// TodoListView.prototype.removeTodo = function(todo) {
+//   this.todoList.removeTodo(todo);
+//   this.render();
+// };
+
+// TodoListView.prototype.removeCompletedTodos = function() {
+//   this.todoList.removeCompletedTodos();
+//   this.render();
+// };
--- todo_view.js
+++ todo_view.js
@@ -1,58 +1,65 @@
 // TODO:
-// Then use mustache templates to replace HTML creating code.
-// Read about CSS selector speed.
 // SUPER MEGA EXTRA CREDIT make a branch that converts to Backbone!!!!
 
 
 // View of an Individual Todo Item
-var TodoView = function(todo) {
-  this.todo = todo;
-  this.$element = this.$buildElement();
-  this.addEvents();
-  return this;
-};
+// var TodoView = function(todo) {
+//   this.todo = todo;
+//   this.$element = this.$buildElement();
+//   this.addEvents();
+//   return this;
+// };
 
-TodoView.prototype.$buildElement = function() {
-  var template = '<li data-id={{id}} {{#completed}}class="complete"{{/completed}}>' +
-                 '<input type="checkbox" {{#completed}}checked{{/completed}}/>' +
-                 '<label>{{title}}</label></li>'
-  var element = Mustache.render(template, this.todo);
-  return $(element);
-};
+// TodoView.prototype.$buildElement = function() {
+//   var template = '<li data-id={{id}} {{#completed}}class="complete"{{/completed}}>' +
+//                  '<input type="checkbox" {{#completed}}checked{{/completed}}/>' +
+//                  '<label>{{title}}</label></li>'
+//   var element = Mustache.render(template, this.todo);
+//   return $(element);
+// };
 
-TodoView.prototype.addEvents = function() {
-  this.$element.find("input").on("click", this.toggleComplete.bind(this));
-  this.$element.find("label").on("dblclick", this.editTitle.bind(this));
-};
+// TodoView.prototype.addEvents = function() {
+//   this.$element.find("input").on("click", this.toggleComplete.bind(this));
+//   this.$element.find("label").on("dblclick", this.editTitle.bind(this));
+// };
 
-TodoView.prototype.toggleComplete = function() {
-  this.$element.toggleClass("complete");
-  this.todo.toggleCompleted();
-};
+// TodoView.prototype.toggleComplete = function() {
+//   this.$element.toggleClass("complete");
+//   this.todo.toggleCompleted();
+// };
 
-TodoView.prototype.editTitle = function() {
-  var template = '<input type="text" value="{{title}}"/>'
-  var newElement = Mustache.render(template, this.todo);
-  this.$element.html(newElement);
-  var editField = this.$element.find("input");
-  editField.focus();
-  editField.on("keypress", this.enterNewTodo.bind(this));
-  editField.on("blur", this.removeEditing.bind(this));
-};
+// TodoView.prototype.editTitle = function() {
+//   var template = '<input type="text" value="{{title}}"/>'
+//   var newElement = Mustache.render(template, this.todo);
+//   this.$element.html(newElement);
+//   var editField = this.$element.find("input");
+//   editField.focus();
+//   editField.on("keypress", this.enterNewTodo.bind(this));
+//   editField.on("blur", this.removeEditing.bind(this));
+// };
 
-TodoView.prototype.enterNewTodo = function(e) {
-  var editField = this.$element.find("input");
-  var newTodoTitle = editField.val().trim();
-  var ENTER_KEY = 13;
-  if (e.keyCode === ENTER_KEY && newTodoTitle !== "") {
-    this.todo.editTitle(newTodoTitle);
-    editField.blur();
-  };
-};
+// TodoView.prototype.enterNewTodo = function(e) {
+//   var editField = this.$element.find("input");
+//   var newTodoTitle = editField.val().trim();
+//   var ENTER_KEY = 13;
+//   if (e.keyCode === ENTER_KEY && newTodoTitle !== "") {
+//     this.todo.editTitle(newTodoTitle);
+//     editField.blur();
+//   };
+// };
 
-TodoView.prototype.removeEditing = function() {
-  var newElement = this.$buildElement();
-  this.$element.replaceWith(newElement);
-  this.$element = newElement;
-  this.addEvents();
-};
+// TodoView.prototype.removeEditing = function() {
+//   var newElement = this.$buildElement();
+//   this.$element.replaceWith(newElement);
+//   this.$element = newElement;
+//   this.addEvents();
+// };
+
+var TodoView = Backbone.View.extend({
+  model: Todo,
+  tagName: "li",
+  template: '<li data-id={{id}} {{#completed}}class="complete"{{/completed}}>' +
+            '<input type="checkbox" {{#completed}}checked{{/completed}}/>' +
+            '<label>{{title}}</label></li>'
+
+});
